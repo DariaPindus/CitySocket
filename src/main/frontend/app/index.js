fcbb3a1d364b4a6295f17b7365a76c3f0f8c65c3
Daria Pindus <daria.pindus@gmail.com>
2018-05-29 00:23:03 +0300

    temp save

--- src/main/frontend/app/index.js
+++ src/main/frontend/app/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/style.css'
 
 ReactDOM.render(
 	<h1 className="testblue">App working</h1>,
-	document.querySelector('.container'));
+	document.querySelector('.container'));*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'app';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.querySelector('.container')
+);
